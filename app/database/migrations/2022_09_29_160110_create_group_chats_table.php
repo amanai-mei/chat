12cd06932254f5cd1c23666b134deffa012276c4
amanai-mei <ampmpm7777@docomo.ne.jp>
2022-09-30 21:31:00 +0900

    first commit

---
+++ app/database/migrations/2022_09_29_160110_create_group_chats_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGroupChatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('group_chats', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('group_id');
+            $table->integer('user_id');
+            $table->string('message', '10000');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('group_chats');
+    }
+}
